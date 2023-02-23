9a03193e7e560aa3dd8d9b68ea88615616739c55
unknown <clementtshepo3@gmail.com>
2023-02-23 11:28:22 +0200

    javascript code

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+let myName = "Clement";
+let mySurname = "Motloutsi";
+let educationalBackground = ["Postgraduate Diploma in Project Management", "Btech in Project Management", "National Diploma in General Management"];
+let myCertificates = ["Aws Cloud Practitioner", "Microsoft Azure Fundamentals", "Fortinet NSE4 Network Security Professional", "CompTIA Security+"];
+let myExperience = ["Webbers Clothing and Footwear-Sales Assistant"];
+let myContact = "0792599573";
+let myEmail = "clementtshepo3@gmail.com";
+
+document.getElementById("para").innerHTML = myName + mySurname + educationalBackground + myCertificates + myExperience
+myExperience + myContact + myEmail;
+
